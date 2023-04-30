9d8291225e433ddce72b0f49baf89ea8ccb6cfd8
kedi1991 <>
2023-04-30 22:47:06 +0000

    Added the files

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "sd6524r2yf34824h24hf74f43")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEVELOPMENT", "True")
+os.environ.setdefault("DB_URL", "postgresql:///taskmanager")
