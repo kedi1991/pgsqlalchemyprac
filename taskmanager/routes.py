8ba82901981c8bb93eef3d98083c8bcffbc47f23
kedi1991 <>
2023-04-30 23:45:52 +0000

    added all files

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,3 +6,13 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/newtask")
+def newtask():
+    return render_template("newtask.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
